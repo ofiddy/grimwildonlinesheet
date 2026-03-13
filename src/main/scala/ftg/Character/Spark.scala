097babb9aa8b494a90575714a771e9c084328a07
ofiddy <108938658+ofiddy@users.noreply.github.com>
2026-03-13 20:24:15 +0000

    Fix spark names

--- src/main/scala/ftg/Character/Spark.scala
+++ src/main/scala/ftg/Character/Spark.scala
@@ -3,13 +3,13 @@ package ftg.Character
 opaque type Spark = Int
 
 extension (s: Spark) {
-  def gainStory: Spark =
+  def gainSpark: Spark =
     if s >= 0 && s < 2 then s + 1 else 2
 
-  def spendStory: Option[Spark] =
+  def spendSpark: Option[Spark] =
     if s <= 0 then None else Some(s - 1)
 }
 
 object Spark {
-  def startingStory: Spark = 2
+  def startingSpark: Spark = 2
 }
