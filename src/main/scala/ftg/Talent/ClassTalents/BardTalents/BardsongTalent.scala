3e94135332587ade4aa522555bc4843281cba675
ofiddy <108938658+ofiddy@users.noreply.github.com>
2026-05-06 20:36:24 +0100

    a POSSIBLE talent setup

---
+++ src/main/scala/ftg/Talent/ClassTalents/BardTalents/BardsongTalent.scala
@@ -0,0 +1,29 @@
+package ftg.Talent.ClassTalents.BardTalents
+
+import ftg.Talent.Markdown
+import ftg.Talent.md
+import ftg.Character.LevelGrowth.given_Growable_Int
+import ftg.Talent.TalentDescriptor
+import ftg.Talent.Talent
+
+object Bardsong {
+  final case class BardsongTalent(bardsongs: Int, melodies: Int)
+      extends Talent {
+    def talentDesc: TalentDescriptor[Talent] = BardsongTalentDescriptor
+  }
+
+  case object BardsongTalentDescriptor
+      extends TalentDescriptor[BardsongTalent] {
+    override def apply(c: ftg.Character.Character) = {
+      given level: Int = c.level
+      val max          = 3 `and every 3 levels` (_ + 1)
+      BardsongTalent(max, max)
+    }
+
+    override def name: String = "Bardsong"
+
+    override def desc: Markdown =
+      "You can sing **3 bardsongs** (*session*). Choose a **Style + Tune of Impact** and roll Presence. Sing one to ***pull off a potent feat of emotional influence*** or ***interrupt*** any impact move. You can also sing **3 melodies** (*session*) without a roll or composition to: *assist without risk*—*calm or intensify a vex response*—*clear a mark*. [Growth: 3 levels/+1 bardsong, +1 melody].".md
+  }
+
+}
--- src/main/scala/ftg/Talent/ClassTalents/BardTalents/FriendlyFaceTalent.scala
+++ src/main/scala/ftg/Talent/ClassTalents/BardTalents/FriendlyFaceTalent.scala
@@ -1,12 +1,17 @@
 package ftg.Talent.ClassTalents.BardTalents
 
-import ftg.Talent.Talent
 import ftg.Talent.Markdown
 import ftg.Talent.md
+import ftg.Talent.StatelessTalent
 
-object FriendlyFace extends Talent {
-  override def freshFromChar(c: ftg.Character.Character): Talent = FriendlyFace
+case object FriendlyFaceTalent extends StatelessTalent {
+
+  override def apply(c: ftg.Character.Character) =
+    FriendlyFaceTalent
+
   override def name: String = "Friendly Face"
+
   override def desc: Markdown =
     "In any new town or district of a city you go to, you can always: *know someone useful*—*quickly make a friend*—*be recognized by a fan*. They’ll gladly do you a reasonable favor. **Take spark** if you promise to pay them back.".md
+
 }
